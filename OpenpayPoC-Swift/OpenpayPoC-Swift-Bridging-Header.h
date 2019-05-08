c5cf552cc7a8a5e3beb9dd98774d1d8c9465d129
Reyes Sanchez Daniel (RBMX/BUD1-IO2) <Daniel.ReyesSanchez@mx.bosch.com>
2019-05-08 07:31:50 -0500

    Openpay integration finished

---
+++ OpenpayPoC-Swift/OpenpayPoC-Swift-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "Openpay.h"
+#import "OPAddress.h"
+#import "OPCard.h"
+#import "OPToken.h"
--- OpenpayPoC-Swift/ViewController.swift
+++ OpenpayPoC-Swift/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let openpay = Openpay()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
